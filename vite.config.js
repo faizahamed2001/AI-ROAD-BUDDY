faeb964d5280dab7b0a472acae714ea5dc9c6a14
Ahamed R <2148514@cognizant.com>
2025-08-14 01:26:23 +0530

    Update project files for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Ensures assets are served from the root
   plugins: [react()],
 })
