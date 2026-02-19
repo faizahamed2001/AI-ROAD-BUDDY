c576cee2aedf644a91ebc5bc12e9471b95f8ba1b
faizahamed2001 <faizahamed2001@gmail.com>
2026-02-19 17:41:40 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+ 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // Ensures assets are served from the root
   plugins: [react()],
+  server: {
+    allowedHosts: ['cts-vibeappuk7603-4.azurewebsites.net', 'localhost', 'cts-vibeappuk7603-4.azurewebsites.net'],
+    port: 8080,    
+    
+    host: true
+  },
 })
